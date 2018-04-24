ee8bc4ab64b86b1644c7461127f549fefe6f454e
yagisuke <yagisuke.0209@gmail.com>
2018-04-25 01:33:01 +0900

    create 1st component

---
+++ src/renderer/components/MarkdownEditorUI.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class MarkDownEditorUI extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, Markdown Editor</h1>
+            </div>
+        )
+    }
+}
+
+export default MarkDownEditorUI
